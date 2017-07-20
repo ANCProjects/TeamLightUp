3ee559039af9e86635da75461e171db811b48f35
Zaks0n <zaksontech@gmail.com>
2017-07-20 15:19:49 +0100

    Added Activity to all the Categories

--- app/src/main/java/com/zaksontech/www/langtutor/MainActivity.java
+++ app/src/main/java/com/zaksontech/www/langtutor/MainActivity.java
@@ -1,7 +1,10 @@
 package com.zaksontech.www.langtutor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView alphabets = (TextView) findViewById(R.id.alphabets);
+        alphabets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent alphabetsIntent = new Intent(MainActivity.this, AlphabetsActivity.class);
+                startActivity(alphabetsIntent);
+            }
+        });
+
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(numbersIntent);
+            }
+        });
+
+        TextView greetings = (TextView) findViewById(R.id.greetings);
+        greetings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent greetingsIntent = new Intent(MainActivity.this, GreetingsActivity.class);
+                startActivity(greetingsIntent);
+            }
+        });
+
+        TextView family = (TextView) findViewById(R.id.family);
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(familyIntent);
+            }
+        });
     }
 }
