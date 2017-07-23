a5a7fd44d6f8fdba3309714807a3e1068b106726
Zaks0n <zaksontech@gmail.com>
2017-07-23 22:32:16 +0100

    Randomized Work

---
+++ app/src/main/java/com/zaksontech/www/langtutor/Word.java
@@ -0,0 +1,25 @@
+package com.zaksontech.www.langtutor;
+
+/**
+ * Created by Zaks0n on 7/23/2017.
+ */
+
+public class Word {
+
+    private String mEnglishTranslation;
+    private String mYorubaTranslation;
+
+
+    public Word(String englishTranslation, String yorubaTranslation){
+        mEnglishTranslation = englishTranslation;
+        mYorubaTranslation = yorubaTranslation;
+    }
+
+    public String getEnglishTranslation(){
+        return mEnglishTranslation;
+    }
+
+    public String getYorubaTranslation(){
+        return mYorubaTranslation;
+    }
+}
